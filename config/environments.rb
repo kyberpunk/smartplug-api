b4819ba5edee725abd7f51c1bc3c9ab79e599dfe
xvhol <xvh@seznam.cz>
2018-02-07 19:20:26 +0100

    Configuration files

--- config/environments.rb
+++ config/environments.rb
@@ -1,21 +1,16 @@
 require 'sinatra'
 
+# Load components configuration
 configure :production, :development do
   environment = ENV['RACK_ENV'] || 'development'
   db_config = YAML.load(File.read("config/database.yml"))
   ActiveRecord::Base.establish_connection(db_config[environment])
 
   iothub_config = YAML.load(File.read("config/iothub.yml"))
-  iothub_options = IotHubOptions.new(iothub_config[:connection_string])
-  iothub_options.expiry = iothub_config[:expiry]
-
+  iothub_options = IotHubOptions.new(iothub_config[environment])
   set :iothub_options, iothub_options
 
-  smartplug_options = SmartplugOptions.new
-  smartplug_options.connect_timeout = 5
-  smartplug_options.direct_data_duration = 30
-  smartplug_options.direct_data_timeout = 10
-  smartplug_options.response_timeout = 5
-
+  smartplug_config = YAML.load(File.read("config/smartplug.yml"))
+  smartplug_options = SmartplugOptions.new(smartplug_config[environment])
   set :smartplug_options, smartplug_options
 end
